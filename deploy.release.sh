31fd517ffaac8250a865196652c6789b4aa2e085
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2019-11-06 11:59:20 +0900

    update scripts

--- deploy.release.sh
+++ deploy.release.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export GAS_DEBUG=0&&
 ./node_modules/.bin/webpack&&
+cp src/appsscript.json dist/appsscript.json&&
 cp .clasp.release.json .clasp.json&&
 clasp push
 cp .clasp.test.json .clasp.json
--- deploy.test.sh
+++ deploy.test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export GAS_DEBUG=1&&
 ./node_modules/.bin/webpack&&
+cp src/appsscript.json dist/appsscript.json&&
 cp .clasp.test.json .clasp.json&&
 clasp push
 export GAS_DEBUG=0
